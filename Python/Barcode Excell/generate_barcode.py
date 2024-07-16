79dc8d4a7350bfefddf1ec9419cc93585f304817
siranjeevicad01 <siranjeevicad01@gmail.com>
2024-07-16 10:34:34 +0530

    generated Barcode in excel folder handle the folder name to save the barcode images every input

--- Python/Barcode Excell/generate_barcode.py
+++ Python/Barcode Excell/generate_barcode.py
@@ -2,18 +2,24 @@ import pandas as pd
 import barcode
 from barcode.writer import ImageWriter
 import os
+import shutil
+from datetime import datetime
 
-def generate_barcode_from_excel(excel_path, output_dir):
-    df = pd.read_excel(excel_path)  
+def generate_barcode_from_excel(excel_path, output_dir, folder_name):
+    df = pd.read_excel(excel_path)
+    new_output_dir = os.path.join(output_dir, folder_name)
     
+
+    if os.path.exists(new_output_dir):
+        shutil.rmtree(new_output_dir)
+        print(f"Existing folder '{new_output_dir}' deleted.")
     
-    os.makedirs(output_dir, exist_ok=True)
+
+    os.makedirs(new_output_dir, exist_ok=True)
     
-  
     for index, row in df.iterrows():
         barcode_number = str(row['Barcode'])  
-        filename = f'{output_dir}/barcode_image_{index + 1}' 
-        
+        filename = os.path.join(new_output_dir, f'barcode_image_{index + 1}') 
         
         PRODUCT = barcode.get_barcode_class('ean13')
         product = PRODUCT(barcode_number, writer=ImageWriter())
@@ -21,8 +27,8 @@ def generate_barcode_from_excel(excel_path, output_dir):
         
         print(f"Barcode {barcode_number} saved as {filename}.png")
 
-
 excel_path = r'D:\PROJECT\cloud_garage_llp\Python\Barcode Excell\barcodes.xlsx' 
-output_dir = r'D:\PROJECT\cloud_garage_llp\Python\Barcode Excell\barcodes'       
+output_dir = r'D:\PROJECT\cloud_garage_llp\Python\Barcode Excell\barcodes'
 
-generate_barcode_from_excel(excel_path, output_dir)
+folder_name = input("Enter Folder name: ")
+generate_barcode_from_excel(excel_path, output_dir, folder_name)
